04a7d27de2047c083eaf7f19e9c1e7ef622f3922
soumya kolekar <80325786+soumya-kolekar@users.noreply.github.com>
2025-01-06 21:13:38 +0530

    Update ext.js

--- ext.js
+++ ext.js
@@ -9,30 +9,3 @@ function toggleMenu(){
     menuToggle.classList.toggle("active");
     menu.classList.toggle("active");
 }
-
-function sendEmail(event) {
-    event.preventDefault(); 
-
-    
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-
-   
-    const templateParams = {
-        user_name: name,
-        user_email: email,
-        message: message,
-    };
-
- 
-    emailjs.send("service_dks8k0w", "template_ockqhyl", templateParams)
-        .then((response) => {
-            alert("Message sent successfully!"); // Notify user of success
-            console.log("Email sent:", response);
-        })
-        .catch((error) => {
-            alert("Failed to send message. Please try again later."); // Notify user of failure
-            console.error("EmailJS Error:", error);
-        });
-}
